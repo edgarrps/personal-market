a3cf2ef8c500c2eecd2b3bf28572743caa959a86
edgarrps <edgarrps@gmail.com>
2023-04-25 21:40:42 -0400

    fiber import

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/edgarrps/personal-market-backend/database"
+import (
+	"log"
+	"os"
+
+	"github.com/edgarrps/personal-market-backend/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	database.Connect()
-
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env files")
+	}
+	port := os.Getenv("PORT")
+	app := fiber.New
 }
