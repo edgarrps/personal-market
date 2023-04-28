1876d671ef46dcceb76b3a003af767b6c6c84cee
edgarrps <edgarrps@gmail.com>
2023-04-27 23:11:10 -0400

    user struct

--- database/connect.go
+++ database/connect.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/edgarrps/personal-market-backend/models"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -20,6 +21,11 @@ func Connect() {
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Couldn't connect to the database")
+	} else {
+		log.Println("Connect sucessfully")
 	}
 	DB = database
+	database.AutoMigrate(
+		&models.User{},
+	)
 }
