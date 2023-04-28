dcc33ed572329ffea555feda181acaabc3c2ac62
edgarrps <edgarrps@gmail.com>
2023-04-28 19:25:38 -0400

    created data var

--- controller/authController.go
+++ controller/authController.go
@@ -1,1 +1,18 @@
 package controller
+
+import (
+	"fmt"
+
+	"github.com/edgarrps/personal-market-backend/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Register(c *fiber.Ctx) error {
+	var data map[string]interface{}
+	var userData models.User
+
+	if err := c.BodyParser(&data); err != nil {
+		fmt.Println("Unable to parse body")
+	}
+
+}
