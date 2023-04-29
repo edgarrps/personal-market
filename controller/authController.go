570bfe04de230ee97798967c0bed5379e1e2b60b
edgarrps <edgarrps@gmail.com>
2023-04-29 17:51:13 -0400

    authcontroller udpate

--- controller/authController.go
+++ controller/authController.go
@@ -2,10 +2,17 @@ package controller
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/edgarrps/personal-market-backend/models"
 	"github.com/gofiber/fiber/v2"
 )
+
+func validateEmail(email string) bool {
+	Re := regexp.MustCompile(`[a-z0-9._%+\-]+@[a-z0-9._%+\-]+\.[a-z0-9._%+\-]`)
+	return Re.MatchString(email)
+}
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]interface{}
@@ -14,5 +21,13 @@ func Register(c *fiber.Ctx) error {
 	if err := c.BodyParser(&data); err != nil {
 		fmt.Println("Unable to parse body")
 	}
+	if len(data["password"].(string)) <= 6 {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Password must be greater than 6 characters",
+		})
+	}
+	if !validateEmail(strings.TrimSpace(data["email"].(string))) {
 
+	}
 }
